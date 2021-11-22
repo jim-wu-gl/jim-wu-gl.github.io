a61e24e8c6d0122a4a8a770226ab987934d3a1a9
jim-wu-gl <jim.wu@greenlight.me>
2021-11-22 13:26:04 -0500

    More updates

---
+++ src/dir/dirSample.js
@@ -0,0 +1,25 @@
+/**
+ * Class to represent something
+ * Some details here
+ */
+class Stuff {
+
+    /**
+     * @param {number} input - an input of some kind
+     */
+    constructor(input) {
+    }
+
+    /**
+     * Does more stuff
+     * @param {number} input - put some number here
+     * @param {string} moreInput - put some letters here
+     * @returns {boolean} always true
+     */
+    doMoreStuff(input, moreInput) {
+        return true;
+    }
+
+}
+
+module.exports = Stuff
